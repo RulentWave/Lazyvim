45fba8cd499f6822b7911d8908b2371d882043fe
Eric Hernandez <eric@hrn.email>
2026-04-06 11:59:06 -0400

    moved config to lua/config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+-- Open terminal in a vertical split
+vim.keymap.set("n", "<leader>tv", function()
+  vim.cmd("vsplit | terminal")
+  vim.cmd("startinsert")
+end, { desc = "Open terminal vertical" })
+
+-- Open terminal in a new tab
+vim.keymap.set("n", "<leader>tt", function()
+  vim.cmd("tabnew | terminal")
+  vim.cmd("startinsert")
+end, { desc = "Open terminal in new tab" })
+
+-- Exit terminal normal mode with double Esc
+vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal normal mode" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,14 @@ vim.o.expandtab = false
 vim.o.clipboard = ""
 vim.o.wrap = true
 vim.g.snacks_animate = false
+--
+-- Set Shell on Windows
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  if vim.fn.executable("nu") == 1 then
+    vim.o.shell = "nu"
+  elseif vim.fn.executable("pwsh") == 1 then
+    vim.o.shell = "pwsh"
+  elseif vim.fn.executable("powershell") == 1 then
+    vim.o.shell = "powershell"
+  end
+end
