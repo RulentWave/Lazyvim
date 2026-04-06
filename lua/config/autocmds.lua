c3ed9e6aba4b5990303f98aa4c5d71050a11901a
Eric Hernandez <eric@hrn.email>
2026-04-06 12:56:41 -0400

    added autocmd for changing the working directory via embded terminals

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,24 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd({ "TermRequest" }, {
+  desc = "Handles OSC 7 dir change requests",
+  callback = function(ev)
+    local val, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
+    if n > 0 then
+      -- OSC 7: dir-change
+      local dir = val
+      if vim.fn.isdirectory(dir) == 0 then
+        dir = string.gsub(dir, "^/", "")
+        if vim.fn.isdirectory(dir) == 0 then
+          vim.notify("invalid dir: " .. val)
+          return
+        end
+      end
+      vim.b[ev.buf].osc7_dir = dir
+      if vim.api.nvim_get_current_buf() == ev.buf then
+        vim.cmd.lcd(dir)
+      end
+    end
+  end,
+})
